09985d1a557e4205d78b1ea4b3fa4cf0881ca134
Nezerwa <eligrand2000@gmail.com>
2023-12-30 18:51:07 +0200

    create user entity

---
+++ carselling/src/user.entity.ts
@@ -0,0 +1,11 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+}
