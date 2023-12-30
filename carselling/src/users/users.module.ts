e379edb7039376d3cb7c7b33902aaf1007a02047
Nezerwa <eligrand2000@gmail.com>
2023-12-30 18:56:03 +0200

    connect user entinty to parent module

--- carselling/src/users/users.module.ts
+++ carselling/src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
